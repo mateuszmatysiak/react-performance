cd59eed91f53c78122997fcfdbb9212a315e3f6a
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2021-07-25 19:10:47 +0200

    Exercise 1 - Code splitting

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,18 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
+
+const prefetchGlobe = () =>
+  import(
+    /* webpackPrefetch: true */
+    /* webpackChunkName: "globe" */
+    '../globe'
+  )
+const Globe = React.lazy(() => prefetchGlobe())
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -26,7 +34,11 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label
+        style={{marginBottom: '1rem'}}
+        // onFocus={prefetchGlobe}
+        // onMouseEnter={prefetchGlobe}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
@@ -35,7 +47,9 @@ function App() {
         {' show globe'}
       </label>
       <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+        <React.Suspense fallback={<div>loading...</div>}>
+          {showGlobe ? <Globe /> : null}
+        </React.Suspense>
       </div>
     </div>
   )
